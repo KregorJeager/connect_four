4bc9e628ea44a6de9948e817694ba83b9ad5196b
kregor <kregorjeager@gmail.com>
2025-11-26 21:28:24 +0800

    Add test template

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal:false
+
+require_relative '../lib/connect_four'
+
+describe ConnectFour do
+  describe '#initialize' do
+    context 'When class is initialized' do
+    end
+  end
+end
