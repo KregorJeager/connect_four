f96d904dce75f7935b66e96ec0109f33b7c502cf
kregor <kregorjeager@gmail.com>
2025-11-27 19:00:38 +0800

    Add test for Board #place_move

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,4 +16,24 @@ describe Board do
       end
     end
   end
+
+  describe '#place_move' do
+    subject(:board_move) { described_class.new }
+    context 'When a move is placed given a x,y coordinate by player_O' do
+      it 'Place move in 3,3' do
+        player_o = 'O'
+        x_coordinate = 3
+        y_coordinate = 3
+        board_move.place_move(x_coordinate, y_coordinate, player_o)
+        grid = board_move.grid
+
+        expect(grid).to eq([[nil, nil, nil, nil, nil, nil, nil],
+                            [nil, nil, nil, nil, nil, nil, nil],
+                            [nil, nil, nil, nil, nil, nil, nil],
+                            [nil, nil, nil, 'O', nil, nil, nil],
+                            [nil, nil, nil, nil, nil, nil, nil],
+                            [nil, nil, nil, nil, nil, nil, nil]])
+      end
+    end
+  end
 end
